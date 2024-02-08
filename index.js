2f25b0f5368fe89b310124fcdb34ba576c60428e
jpedrocrvg <137358244+jpedrocrvg@users.noreply.github.com>
2024-02-08 13:31:11 -0300

    index.js

--- index.js
+++ index.js
@@ -52,3 +52,58 @@ document.addEventListener('keydown', function(event) {
         buttonAnimation('l', 'pressed');
     }
 });
+
+// Função para reproduzir o áudio
+function playAudio(audioId) {
+    var audio = document.getElementById(audioId);
+    audio.play();
+}
+
+// Função para animar o botão
+function buttonAnimation(buttonId, classButton) {
+    var pressedButton = document.getElementById(buttonId);
+    if (pressedButton) {
+        pressedButton.classList.add(classButton);
+
+        setTimeout(function() {
+            pressedButton.classList.remove(classButton);
+        }, 100); 
+    } 
+}
+
+
+document.getElementById('w').addEventListener('click', function() {
+    playAudio('crash');
+    buttonAnimation('w', 'pressed');
+});
+
+document.getElementById('a').addEventListener('click', function() {
+    playAudio('kick-bass');
+    buttonAnimation('a', 'pressed');
+});
+
+document.getElementById('s').addEventListener('click', function() {
+    playAudio('snare');
+    buttonAnimation('s', 'pressed');
+});
+
+document.getElementById('d').addEventListener('click', function() {
+    playAudio('tom-1');
+    buttonAnimation('d', 'pressed');
+});
+
+document.getElementById('j').addEventListener('click', function() {
+    playAudio('tom-2');
+    buttonAnimation('j', 'pressed');
+});
+
+document.getElementById('k').addEventListener('click', function() {
+    playAudio('tom-3');
+    buttonAnimation('k', 'pressed');
+});
+
+document.getElementById('l').addEventListener('click', function() {
+    playAudio('tom-4');
+    buttonAnimation('l', 'pressed');
+});
+
